4e8f42970f7b5626c48c5f5d7f0e1a15f980d026
s7a73farm <s7a73farm@gmail.com>
2021-01-29 10:51:23 -0700

    Update Dashboard.vue
    Added Webcam and Macros

--- src/routes/Control/Dashboard.vue
+++ src/routes/Control/Dashboard.vue
@@ -2,29 +2,11 @@
 	<v-row>
 		<v-col xs="12" sm="8" md="8" lg="9" xl="9">
 			<movement-panel class="mb-3"></movement-panel>
-
-			<v-row v-if="isFFForUnset" class="my-3">
-				<v-col sm="12" :md="(atxPower !== null) ? 9 : 12" :lg="(atxPower !== null) ? 9 : 12" :xl="(atxPower !== null) ? 10 : 12">
-					<extrude-panel></extrude-panel>
-				</v-col>
-
-				<v-col v-if="atxPower !== null" md="3" lg="3" xl="2" align-self="center">
-					<atx-panel></atx-panel>
-				</v-col>
-			</v-row>
-
-			<v-row class="my-3">
-				<v-col sm="12" :md="(!isFFForUnset && atxPower !== null) ? 9 : 12" :lg="(!isFFForUnset && atxPower !== null) ? 9 : 12" :xl="(!isFFForUnset && atxPower !== null) ? 10 : 12">
-					<fan-panel></fan-panel>
-				</v-col>
-
-				<v-col v-if="!isFFForUnset && atxPower !== null" md="3" lg="3" xl="2" align-self="center">
-					<atx-panel></atx-panel>
-				</v-col>
-			</v-row>
+			<extrude-panel></extrude-panel>
+			<fan-panel></fan-panel>
 		</v-col>
-
-		<v-col class="hidden-xs-only" sm="4" md="4" lg="3" xl="3">
+		<v-col xs="4" sm="4" md="4" lg="3" xl="3">
+			<webcam-panel></webcam-panel>
 			<macro-list></macro-list>
 		</v-col>
 	</v-row>
